ec5907ac04c140d8ec9774308be3a361835ecda2
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-10-28 13:00:33 -0700

    Update info.js

--- info.js
+++ info.js
@@ -1,12 +1,12 @@
 function maxMinAvg(arr) {
     var max = arr[0];
     var min = arr[0];
-    var sum = arr[0];
+    var sum = 0;
     for (var i=1; i<arr.length; i++){
-        if(arr[0]>max){
+        if(arr[i]>max){
             max = arr[0];
         }
-        if(arr[0]<min){
+        if(arr[i]<min){
             min = arr[0];
         }
         sum = sum + arr[i];
