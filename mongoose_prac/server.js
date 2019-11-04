6b18bd997d18135c4ce711bbc1b17ab7ec27eafc
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-04 12:45:43 -0800

    Update server.js

---
+++ mongoose_prac/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+
+// app.get('/', (req, res) => {
+//    res.send("Hello Express");
+// });
+
+app.use(bodyParser.urlencoded({entended: true}));
+app.use(express.static(__dirname + "/static"));
+
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+
+mongoose.connect('mongodb://localhost/users', {useNewUrlParser: true});
+const UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+   })
+   // create an object to that contains methods for mongoose to interface with MongoDB
+   const User = mongoose.model('User', UserSchema);
+
+app.get("/", function(req, res){
+    res.render("index");
+})
+
+
+app.post('/users', (req, res)=>{
+    const user = new User();
+    user.name = req.body.name;
+    user.age = req.body.age;
+    user.save()
+    .then(newUserData => console.log('user created: ', newUserData))
+    .catch(err => console.log(err));
+
+    res.redirect('/');
+})
+
+app.get('/', (req, res) =>{
+    User.find()
+        .then(data => res.render("index", {users: data}))
+        .catch(err => res.json(err));
+});
+
+app.listen(8000, () => console.log("listening on port 8000"));
