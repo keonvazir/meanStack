28bd8584bff045fac309a6e2d8be76f67e4cbbef
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-06 15:44:34 -0800

    Update routes.js

---
+++ quote_dojo_modularize/server/config/routes.js
@@ -0,0 +1,12 @@
+const quotes = require("../controllers/quotes.js");
+
+
+module.exports = function(app){
+
+    app.get('/', quotes.index)
+
+    app.get('/quotes', quotes.quotePage)
+
+    app.post('/quotes', quotes.addQuote)
+
+};        
