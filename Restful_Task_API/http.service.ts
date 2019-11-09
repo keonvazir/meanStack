536f42668dfba67442c59120c98b2d8f37806872
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-08 16:38:38 -0800

    Update http.service.ts

--- Restful_Task_API/http.service.ts
+++ Restful_Task_API/http.service.ts
@@ -11,11 +11,37 @@ export class HttpService {
   constructor(private _http: HttpClient) { 
     // this.getTasks();
     this.getPokemon();
+    this.pokemonAbilities();
+    this.overgrow();
+    this.chlorophyll();
   }
 
   getPokemon(){
     let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
 
     bulbasaur.subscribe(data => console.log("Got our tasks!", data));
-  }
+
+
+  };
+
+  pokemonAbilities(){
+    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+
+    bulbasaur.subscribe((data:any)=>{
+      console.log(`${data.name}'s abilities are ${data.abilities[0].ability.name} and ${data.abilities[1].ability.name}.`);
+    })
+
+  };
+  overgrow(){
+    let overgrow = this._http.get('https://pokeapi.co/api/v2/ability/65/');
+    overgrow.subscribe((data:any)=>{
+      console.log(`${data.bulbasaur.length} Pokemon have the ${data.name} ability!`);
+    })
+  };
+  chlorophyll(){
+    let chlorophyll = this._http.get('https://pokeapi.co/api/v2/ability/34/');
+    chlorophyll.subscribe((data:any)=>{
+      console.log(`${data.bulbasaur.length} Pokemon have the ${data.name} ability!`);
+    })
+  };
 }
