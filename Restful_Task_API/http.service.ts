f4183f2d24e0bd976cd95e892531b9d2ee4bf255
keonvazir <54293518+keonvazir@users.noreply.github.com>
2019-11-08 16:15:17 -0800

    Create http.service.ts

---
+++ Restful_Task_API/http.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  constructor(private _http: HttpClient) { 
+    // this.getTasks();
+    this.getPokemon();
+  }
+
+  getPokemon(){
+    let bulbasaur = this._http.get('https://pokeapi.co/api/v2/pokemon/1/');
+
+    bulbasaur.subscribe(data => console.log("Got our tasks!", data));
+  }
+}
